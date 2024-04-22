610e35a373e90757a4133c8810d790fd23ddcf56
Anthony Eufemio <ace@135.io>
2024-04-22 13:09:30 -1000

    add more coins

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
+use chrono::{DateTime, Local, Utc};
+use colorism::{foreground::Fore, util::RESET};
+use daemonize::Daemonize;
+use influxdb::{Client, Error, InfluxDbWriteable, ReadQuery};
+use rand::Rng;
+use reqwest;
+use serde::Deserialize;
+use std::env;
 use std::fs::File;
 use std::io::Read;
-use chrono::{DateTime, Local, Utc};
-use influxdb::{Client, InfluxDbWriteable, ReadQuery, Error};
-use rand::Rng;
-use serde::Deserialize;
-use tokio::time::{sleep, Duration};
-use reqwest;
-use std::env;
 use std::path::Path;
-use daemonize::Daemonize;
-use colorism::{foreground::Fore, util::RESET};
+use tokio::time::{sleep, Duration};
 
 fn main() -> Result<(), failure::Error> {
-
     let file_path_output = format!("{}/.cmc_influx/output.log", env::var("HOME").unwrap());
     let file_path_error = format!("{}/.cmc_influx/error.log", env::var("HOME").unwrap());
     let file_path_pid = format!("{}/.cmc_influx/pid", env::var("HOME").unwrap());
@@ -28,23 +27,19 @@ fn main() -> Result<(), failure::Error> {
         .chown_pid_file(true)
         .pid_file(file_path_pid);
 
-
     match daemonize.start() {
         Ok(_) => {
             println!("Success, daemonized");
             fetch_main(cmc_password.as_str());
             Ok(())
-        },
-        Err(_) => todo!()
+        }
+        Err(_) => todo!(),
     }
-
 }
-
 
 #[tokio::main]
 pub async fn fetch_main(cmc_api_key: &str) -> Result<(), Error> {
     loop {
-
         let local: DateTime<Local> = Local::now();
         let timestamp = format!(
             "{} {} {}",
@@ -52,13 +47,19 @@ pub async fn fetch_main(cmc_api_key: &str) -> Result<(), Error> {
             local.to_string(),
             RESET
         );
-        println!("[{}] Waking up to get data and persist to InfluxDB", timestamp);
+        println!(
+            "[{}] Waking up to get data and persist to InfluxDB",
+            timestamp
+        );
         let listing = CryptoListing::fetch_web(&cmc_api_key);
-        listing.await.persist_db().await.expect("TODO: panic message");
+        listing
+            .await
+            .persist_db()
+            .await
+            .expect("TODO: panic message");
         sleep(Duration::from_millis(60000)).await;
     }
 }
-
 
 async fn read_data(series: &str) -> Result<(), Error> {
     let client = Client::new("http://localhost:8086", "test");
@@ -87,13 +88,11 @@ pub struct CryptoData {
     self_reported_market_cap: Option<f64>,
     tvl_ratio: Option<f64>,
     last_updated: String,
-    quote: CryptoQuote
+    quote: CryptoQuote,
 }
 
 #[derive(Deserialize, Debug, Clone)]
-pub struct CryptoPlatform {
-
-}
+pub struct CryptoPlatform {}
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct CurrencyQuote {
@@ -110,12 +109,12 @@ pub struct CurrencyQuote {
     market_cap_dominance: f64,
     fully_diluted_market_cap: f64,
     tvl: Option<f64>,
-    last_updated: String
+    last_updated: String,
 }
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct CryptoQuote {
-    USD: CurrencyQuote
+    USD: CurrencyQuote,
 }
 
 #[derive(Deserialize, Debug, Clone)]
@@ -126,61 +125,62 @@ pub struct CryptoListingStatus {
     elapsed: i32,
     credit_count: i32,
     notice: Option<String>,
-    total_count: i32
+    total_count: i32,
 }
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct CryptoListing {
-   status: CryptoListingStatus,
-    data: Vec<CryptoData>
+    status: CryptoListingStatus,
+    data: Vec<CryptoData>,
 }
 
 impl CryptoListing {
     pub async fn fetch(file: &str) -> CryptoListing {
-       let mut fi = File::open(file).unwrap();
+        let mut fi = File::open(file).unwrap();
         let mut data = String::new();
         fi.read_to_string(&mut data).unwrap();
-        let cryptolisting: CryptoListing = serde_json::from_str(&data).expect("JSON was not well-formatted");
+        let cryptolisting: CryptoListing =
+            serde_json::from_str(&data).expect("JSON was not well-formatted");
         cryptolisting
     }
 
     pub async fn fetch_web(api_key: &str) -> CryptoListing {
         let client = reqwest::Client::new();
-        let cmc_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=500&convert=USD";
-        let res =
-            client
-                .get(cmc_url)
-                .header("X-CMC_PRO_API_KEY", api_key)
-                .send()
-                .await
-                .expect("failed to get response")
-                .text()
-                .await
-                .expect("failed to get payload");
-        let cryptolisting: CryptoListing = serde_json::from_str(&res).expect("JSON was not well-formatted");
+        let cmc_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5000&convert=USD";
+        let res = client
+            .get(cmc_url)
+            .header("X-CMC_PRO_API_KEY", api_key)
+            .send()
+            .await
+            .expect("failed to get response")
+            .text()
+            .await
+            .expect("failed to get payload");
+        let cryptolisting: CryptoListing =
+            serde_json::from_str(&res).expect("JSON was not well-formatted");
         cryptolisting
         //let cryptolisting: CryptoListing = serde_json::from_str(res.unwrap()).expect("JSON was not well-formatted");
         //cryptolisting
     }
 
     pub async fn persist_db(&self) -> Result<(), Error> {
-
         for data in self.data.iter() {
-           let cmc_price = CmcPrice {
-               time: chrono::offset::Utc::now(),
-               price: data.quote.USD.price,
-               volume: data.quote.USD.volume_24h,
-               market_cap: data.quote.USD.market_cap,
-               hour_change: data.quote.USD.percent_change_1h,
-               day_change: data.quote.USD.percent_change_24h,
-               week_change: data.quote.USD.percent_change_7d,
-               month_change: data.quote.USD.percent_change_30d,
-               quarter_change: data.quote.USD.percent_change_90d
-           };
-            cmc_price.insert_to_db(&data.symbol.to_string().to_lowercase()).await.expect("TODO: panic message");
-
+            let cmc_price = CmcPrice {
+                time: chrono::offset::Utc::now(),
+                price: data.quote.USD.price,
+                volume: data.quote.USD.volume_24h,
+                market_cap: data.quote.USD.market_cap,
+                hour_change: data.quote.USD.percent_change_1h,
+                day_change: data.quote.USD.percent_change_24h,
+                week_change: data.quote.USD.percent_change_7d,
+                month_change: data.quote.USD.percent_change_30d,
+                quarter_change: data.quote.USD.percent_change_90d,
+            };
+            cmc_price
+                .insert_to_db(&data.symbol.to_string().to_lowercase())
+                .await
+                .expect("TODO: panic message");
         }
-
 
         Ok(())
     }
@@ -211,7 +211,7 @@ impl CmcPrice {
             market_cap: rng.gen(),
             price: rng.gen(),
             volume: rng.gen(),
-            quarter_change: rng.gen()
+            quarter_change: rng.gen(),
         }
     }
 
@@ -221,5 +221,4 @@ impl CmcPrice {
         client.query(cmc_prices).await?;
         Ok(())
     }
-
 }
